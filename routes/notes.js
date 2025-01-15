341ee6557d523a1efdb62338e99515065062e712
Yoko Koyama <yokokoyama@YokonoMacBook-Air.local>
2025-01-15 18:51:25 +0900

    Add mongodb access

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "********";
+const uri = "mongodb+srv://********:blhH07vR3ZItE2kL@cluster0.14ov8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
