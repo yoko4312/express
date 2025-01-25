9407acbc7493dec608754ab790b2705405e87e59
Yoko Koyama <yokokoyama@YokonoMacBook-Air.local>
2025-01-25 10:11:02 +0900

    add env

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const cors = require('cors'); // corsミドルウェアを追加
 require('dotenv').config();
 
 
@@ -8,14 +9,17 @@ const { MongoClient } = require("mongodb");
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
+// corsミドルウェアを使用
+router.use(cors());
+
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
 const database = client.db('notes');
 const notes = database.collection('notes');
 
-// idが１のドキュメントを取得
-const query = { id: 2 };
-const note = await notes.findOne(query);
+
+// 全てのドキュメントを取得
+const note = await notes.find({}).toArray();
 
 res.json(note);
 })
